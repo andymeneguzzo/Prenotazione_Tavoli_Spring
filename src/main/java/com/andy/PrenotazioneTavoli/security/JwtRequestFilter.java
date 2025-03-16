82502081ed74bdf2939b05adb1987f444d01bc8d
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-16 01:12:08 +0100

    JwtRequestFilter create and todo

---
+++ src/main/java/com/andy/PrenotazioneTavoli/security/JwtRequestFilter.java
@@ -0,0 +1,25 @@
+package com.andy.PrenotazioneTavoli.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // finish implementation
+    }
+}
