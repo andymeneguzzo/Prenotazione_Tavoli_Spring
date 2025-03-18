1672201c7e0e04f9116e2105fe791358a2731cc5
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-18 09:35:11 +0100

    Utente class

--- src/main/java/com/andy/PrenotazioneTavoli/entity/Utente.java
+++ src/main/java/com/andy/PrenotazioneTavoli/entity/Utente.java
@@ -11,12 +11,13 @@ public class Utente {
 
     private String nome;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
+
     private String password;
+
     private String ruolo;
 
-    // Costruttore vuoto per JPA
     public Utente() {}
 
     public Utente(String nome, String email, String password, String ruolo) {
@@ -26,34 +27,39 @@ public class Utente {
         this.ruolo = ruolo;
     }
 
-    // Getters e Setters
-    public Long getId() {return id;}
+    // Getter & Setter
+    public Long getId() {
+        return id;
+    }
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getNome() {return nome;}
+    public String getNome() {
+        return nome;
+    }
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public String getEmail() {return email;}
+    public String getEmail() {
+        return email;
+    }
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getPassword() {return password;}
+    public String getPassword() {
+        return password;
+    }
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getRuolo() {return ruolo;}
+    public String getRuolo() {
+        return ruolo;
+    }
     public void setRuolo(String ruolo) {
         this.ruolo = ruolo;
-    }
-
-    @Override
-    public String toString() {
-        return "Utente [id=" + id + ", nome=" + nome + ", email=" + email + ", ruolo=" + ruolo + "]";
     }
 }
